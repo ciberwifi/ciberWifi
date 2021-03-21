7bbc3990a64a174d41e160e2c1b8b76236af5767
LauraSO <lauraferreri@live.com.ar>
2021-03-20 21:28:16 -0300

    Se agrega funcionalidad Diagnosticos
    alta diagnostico- ok
    ver diagnostico-ok
    cerrar reclamo - en proceso

--- php/base.php
+++ php/base.php
@@ -2,6 +2,7 @@
 
  require("clases/base/Cliente.php");
  require("clases/base/Reclamo.php");
+ require("clases/base/Diagnostico.php");
 
  $urlBD="Z:/redes.bas/baseDatos/";
  
@@ -87,6 +88,15 @@ function grabarEnTablaReclamos($array){
 
 }
 
+
+function grabarEnTablaDiagnosticos($array){
+
+	$tablaDiagnosticos="Z:/redes.bas/baseDatos/"."tablas/reclamos/tablaDiagnosticos".date('m')."-".date('y').".csv";
+
+	grabarEnTabla($tablaDiagnosticos, $array);
+
+}
+
 function leertablaReclamos(){
 
 	$tablaReclamos="Z:/redes.bas/baseDatos/"."tablas/reclamos/tablaReclamos".date('m')."-".date('y').".csv";
@@ -103,5 +113,23 @@ function leertablaReclamos(){
 
      return($arrayDatos);
 }
+
+function leertablaDiagnosticos(){
+
+	$tablaDiagnosticos="Z:/redes.bas/baseDatos/"."tablas/reclamos/tablaDiagnosticos".date('m')."-".date('y').".csv";
+
+
+	$vecTabla=file($tablaDiagnosticos);
+	$arrayDatos=array();
+	 
+	 foreach($vecTabla as $linea ) {
+		$dato = explode(",", $linea);
+		$diagnostico=new Diagnostico($dato);
+		array_push($arrayDatos, $diagnostico);
+     }
+
+     return($arrayDatos);
+}
+
 
 ?>
--- php/reclamosPendientes.php
+++ php/reclamosPendientes.php
@@ -6,12 +6,12 @@ require("base.php");
 
 $arrayDatos=array();
 
-$arrayDatos=leertablaDiagnosticos();
+$arrayDatos=leertablaReclamos();
 
 
 ?>
 
-<h4 style="margin-bottom: 30px;">Diagnosticos</h4>
+<h4 style="margin-bottom: 30px;">Reclamos Pendientes</h4>
 
 
 
@@ -21,13 +21,12 @@ $arrayDatos=leertablaDiagnosticos();
         <table class="table table-striped table-sm">
           <thead>
             <tr>
-              <th>Fecha Diagnostico</th>
+              <th>Fecha</th>
               <th>IP</th>
-              <th>Motivo Falla</th>
-              <th>Revision en Domicilio</th>
-              <th>En Monitoreo</th>
+              <th>Motivo</th>
+              <th>Tiene estabilizador</th>
               <th>Datos operador</th>
-             
+               <th>Estado</th>
                 <th></th>
             </tr>
           </thead>
@@ -38,11 +37,16 @@ $arrayDatos=leertablaDiagnosticos();
               <td> <?php echo $dato->getfecha();?> </td>
               <td><?php echo $dato->getip();?></td>
               <td><?php echo $dato->getmotivo();?></td>
-              <td><?php echo $dato->getmonitoreo();?></td>
-              <td><?php echo $dato->getvisitaTecnica();?></td>
+              <td><?php echo $dato->getestabilizador();?></td>
               <td><?php echo $dato->getobservaciones();?></td>
-          
-             
+              <td><?php echo $dato->getestado();?></td>
+              <td>
+                <button type="button" class="btn btn-primary" id="btndiagnosticos"  data-bs-toggle="modal" data-bs-target="#exampleModal" >Diagnosticar </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-primary" id="btncerrarticket"  data-bs-toggle="modal" data-bs-target="#exampleModal" >Cerrar Ticket</button>
+              </td>
+              </tr>
             <?php
               }
               ?>
@@ -54,7 +58,7 @@ $arrayDatos=leertablaDiagnosticos();
       </div>
 
 
-      <div id="resultado2" class="container" style="margin-top: 20px;" >
+      <div id="resultado3" class="container" style="margin-top: 20px;" >
 
 
    
@@ -72,7 +76,7 @@ $arrayDatos=leertablaDiagnosticos();
    $("#btndiagnosticos").click(function(){
 
       var loadUrl = "html/nuevodiagnostico.html"; // paso parametro accion e id
-      $("#resultado2").load(loadUrl); // ejecuto
+      $("#resultado3").load(loadUrl); // ejecuto
       }); 
 
   }); 
