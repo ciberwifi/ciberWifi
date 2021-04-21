e0cff02744107eacac3144eb0bea57ed927a430d
LauraSO <lauraferreri@live.com.ar>
2021-04-21 19:49:08 -0300

    busqueda cliente completa
    -dni
    -idip
    -direccion
    -apellido y nombre
    
    parcial

--- php/resultado.php
+++ php/resultado.php
@@ -7,13 +7,52 @@ require_once "../bootstrap.php";
 require_once "../Model/Cliente.php";
 
 
- $id= htmlspecialchars($_POST['imptDni']);
+ $dni= htmlspecialchars($_POST['imptDni']);
+ 
 
-$cliente = $entityManager->findBy('Cliente', $id);
+
+$clientes=array();
+
+
+
+
+if (sizeof($clientes) === 0) {
+$qb = $entityManager->createQueryBuilder();
+$clientes=$qb->select('c')
+   ->from('Cliente', 'c')
+   ->where($qb->expr()->like('c.apellidoynombre', ':element'))
+   ->setParameter('element', '%' . $dni . '%')
+   ->getQuery()
+  ->getResult()
+;
+
+if (sizeof($clientes) === 0){
+$qb = $entityManager->createQueryBuilder();
+   $clientes=$qb->select('c')
+   ->from('Cliente', 'c')
+   ->where($qb->expr()->like('c.direccion', ':element'))
+   ->setParameter('element', '%' . $dni . '%')
+   ->getQuery()
+  ->getResult()
+;
+
+}
+}
+
+$cliente = $entityManager->getRepository('Cliente')->findOneBy(array('idip' => $dni));
 
 
 if ($cliente === null) {
-    echo "No product found.\n";
+$cliente = $entityManager->getRepository('Cliente')->findOneBy(array('dni' => $dni));
+}
+
+if ($cliente !==null) {
+array_push($clientes,$cliente);
+}
+
+
+if (sizeof($clientes) === 0) {
+    echo "El cliente selecionado no existe.\n";
     exit(1);
 }
 
@@ -21,7 +60,11 @@ if ($cliente === null) {
 
 ?>
 
-<h4>Cliente</h4>
+<h4>Clientes</h4>
+
+
+
+
 
 <span class="border-bottom"></span>
 
@@ -29,24 +72,38 @@ if ($cliente === null) {
         <table class="table table-striped table-sm">
           <thead>
             <tr>
+              <th>Zona</th>
               <th>IP</th>
               <th>Apellido y Nombre</th>
-              <th>direccion</th>
-              <th>Plan</th>
-              <th>Mensual</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td> <?php echo $cliente->getidip();?> </td>
-              <td><?php echo $cliente->getapellidoynombre();?></td>
-              <td><?php echo $cliente->getdireccion();?></td>
+              <th>Dni</th>
+              <th>Telefonos</th>
+              <th>Direccion</th>
+              <th>Medios Pago</th>
               
             </tr>
-          <tr>
+          </thead>
+         
+         <tbody>
+             <?php foreach($clientes as $cliente ) { ?>
+            <tr>
+              <td> <?php echo $cliente->getzona();?> </td>
+              <td> <?php echo $cliente->getidip();?> </td>
+              <td><?php echo $cliente->getapellidoynombre();?></td>
+              <td><?php echo $cliente->getdni();?></td>
+              <td><?php echo $cliente->gettel();?></td>
+              <td><?php echo $cliente->getdireccion();?></td>
+              <td><?php echo $cliente->getmediospago();?></td>
+              
+            </tr>
+            <?php } ?>
+      
           </tbody>
         </table>
       </div>
+
+
+
+
 
 
 
